603a285275b0252e87a38b734b3bed8cdffe531c
Sean Pollock <seanpollock3344@gmail.com>
2017-03-29 12:36:28 -0500

    Getting simple line to render

--- src/LineChart.js
+++ src/LineChart.js
@@ -2,27 +2,29 @@ import React, { Component } from 'react';
 
 import {
   ART,
+  View
 } from 'react-native';
 
 const {
   Group,
   Shape,
-  Surface,
+  Surface
 } = ART;
 
 export default class LineChart extends Component {
   render() {
-    console.log('Surface', Surface);
     return (
-      <Surface width={200} heigh={100}>
-        <Group>
-          <Shape
-            d={this.props.linePath}
-            stroke="#000"
-            strokeWidth={1}
-          />
-        </Group>
-      </Surface>
+      <View>
+        <Surface width={200} height={200}>
+          <Group x={0} y={0}>
+            <Shape
+              d="M10 80 C 40 10, 65 10, 95 80 S 150 150, 180 80"
+              stroke="#000"
+              strokeWidth={1}
+            />
+          </Group>
+        </Surface>
+      </View>
     )
   }
 }
