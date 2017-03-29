869782e3e6bb569d0aa03fe2fdb7a18ca1081acf
Sean Pollock <seanpollock3344@gmail.com>
2017-03-29 16:58:02 -0500

    LineChart now using mock data

--- src/LineChart.js
+++ src/LineChart.js
@@ -19,21 +19,21 @@ export default class LineChart extends Component {
   render() {
     console.log('weather_data', weather_data.daily.data);
     const size = 200;
-    console.log('createLineGraph', createLineGraph);
-    // const data = createLineGraph({
-    //   data: weather_data.daily.data,
-    //   width: size,
-    //   height: size,
-    //   xAccessor: d => new Date(d.time * 1000),
-    //   yAccessor: d => d.temperatureMax
-    // });
+    // console.log('createLineGraph', createLineGraph);
+    const data = createLineGraph({
+      data: weather_data.daily.data,
+      width: size,
+      height: size,
+      xAccessor: d => new Date(d.time * 1000),
+      yAccessor: d => d.temperatureMax
+    });
 
     return (
       <View>
         <Surface width={200} height={200}>
           <Group x={0} y={0}>
             <Shape
-              d="M10 80 C 40 10, 65 10, 95 80 S 150 150, 180 80"
+              d={data.path}
               stroke="#000"
               strokeWidth={1}
             />
