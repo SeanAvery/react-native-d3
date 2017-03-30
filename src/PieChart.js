8b38a523a9e94a568e6cfa7c6b5b8a8abe091855
Sean Pollock <seanpollock3344@gmail.com>
2017-03-30 12:14:39 -0500

    Scaffolding out react component for pie chart

---
+++ src/PieChart.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import {
+  ART,
+  View,
+  Text,
+  StyleSheet,
+} from 'react-native';
+
+import data from './data/pie-data';
+console.log('data', data);
+
+export default class PieChart extends Component {
+  render() {
+    return (
+      <View>
+      </View>
+    );
+  }
+}
