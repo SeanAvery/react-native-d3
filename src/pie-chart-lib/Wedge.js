5a97c9d46e09e33772f6e58ec7741fb39cc48438
Sean Pollock <seanpollock3344@gmail.com>
2017-03-30 14:50:15 -0500

    Scaffolding out wedge component

---
+++ src/pie-chart-lib/Wedge.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+  ART,
+  View,
+} from 'react-native';
+const {
+  Shape,
+  Path,
+} = ART;
+
+class Wedge extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    <Shape>
+    </Shape>
+  }
+}
